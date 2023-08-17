e55dcfa65c43d4cec10f687f60d48f36f39e08bd
Suka <mateodefalco1@gmail.com>
2023-08-17 10:58:18 -0300

    Form setting and styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 		projectName: "",
 		projectDescription: "",
 		client: "",
-		contractor: "",
+		constructor: "",
 	})
 	const [csvFile, setCsvFile] = useState(null)
 	const [minMaxValues, setMinMaxValues] = useState({
@@ -26,8 +26,7 @@ function App() {
 	const handleFileUpload = (e) => {
 		const file = e.target.files[0]
 		if (file) {
-			// Implement CSV file parsing here
-			// Update minMaxValues based on parsed data
+			
 			setCsvFile(file)
 		}
 	}
@@ -38,55 +37,48 @@ function App() {
 	}
 
 	return (
-		<div>
+		<div className="flex flex-col items-center justify-center gap-4 p-6">
 			{step === 1 && (
 				<form className="flex flex-col items-center justify-center gap-4 p-6 rounded-lg bg-gray-800 shadow-md">
-					<h1 className="text-2xl font-semibold mb-4">
-						Step 1: Project Information
-					</h1>
-
+					<h1 className="text-white text-2xl font-semibold">XYZ ENGINE</h1>
 					{/* NAME */}
 					<input
-						className="input"
+						className="bg-gray-700 text-white rounded-md p-2"
 						type="text"
 						name="projectName"
 						value={projectInfo.projectName}
 						onChange={handleInputChange}
 						placeholder="Project Name"
 					/>
-
 					{/* DESCRIPTION */}
 					<input
-						className="input"
+						className="bg-gray-700 text-white rounded-md p-2"
 						type="text"
 						name="projectDescription"
 						value={projectInfo.projectDescription}
 						onChange={handleInputChange}
 						placeholder="Description"
 					/>
-
 					{/* CLIENT */}
 					<input
-						className="input"
+						className="bg-gray-700 text-white rounded-md p-2"
 						type="text"
 						name="client"
 						value={projectInfo.client}
 						onChange={handleInputChange}
 						placeholder="Client"
 					/>
-
 					{/* CONTRACTOR */}
 					<input
-						className="input"
+						className="bg-gray-700 text-white rounded-md p-2"
 						type="text"
 						name="constructor"
 						value={projectInfo.constructor}
 						onChange={handleInputChange}
 						placeholder="Constructor"
 					/>
-
 					<button
-						type="submit"
+						type="button"
 						className="btn bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded focus:outline-none focus:ring"
 						onClick={handleNextStep}
 					>
@@ -95,15 +87,111 @@ function App() {
 				</form>
 			)}
 			{step === 2 && (
-				<div className="flex flex-col align-center justify-center">
-					<h2>Step 2: Additional Information</h2>
+				<div className="flex flex-col items-center justify-center gap-4">
+					<h2 className="text-xl font-semibold">
+						Step 2: Additional Information
+					</h2>
 					{/* Display projectInfo values from step 1 */}
-					<p>Project Name: {projectInfo.projectName}</p>
-					{/* Add other input fields for project description, client, and contractor */}
-					<input type="file" onChange={handleFileUpload} />
-					{/* Display minMaxValues values */}
-					<p>Max X: {minMaxValues.max_X}</p>
-					{/* Add other minMaxValues fields */}
+					<div className="p-4 bg-gray-800 rounded-lg shadow-md">
+						<div className="flex gap-1">
+							<p className="text-white">Project Name:</p>
+							<p className="text-white font-bold">{projectInfo.projectName}</p>
+						</div>
+						<div className="flex gap-1">
+							<p className="text-white">Description:</p>
+							<p className="text-white font-bold">
+								{projectInfo.projectDescription}
+							</p>
+						</div>
+						<div className="flex gap-1">
+							<p className="text-white">Client:</p>
+							<p className="text-white font-bold">{projectInfo.client}</p>
+						</div>
+						<div className="flex gap-1">
+							<p className="text-white">Constructor:</p>
+							<p className="text-white font-bold">{projectInfo.constructor}</p>
+						</div>
+					</div>
+					<form className="flex flex-col items-center gap-4 p-4 rounded-xl border border-2 border-gray-800 border-solid">
+						<p className="text-white bg-gray-700 p-2 ">
+							💡 Upload the file, or type them manually
+						</p>
+						<input
+							type="file"
+							className="flex justify-center"
+							onChange={handleFileUpload}
+							accept=".csv"
+						/>
+						{/* Display minMaxValues values */}
+						<div className="flex flex-col gap-2 p-4 bg-gray-800 rounded-lg shadow-md">
+							<div className="flex justify-center items-center gap-1">
+								<label className="text-white min-w-50">Min X:</label>
+								<input
+									className="bg-gray-700 text-white rounded-md p-2"
+									type="text"
+									name="minX"
+									value={minMaxValues.min_X}
+									onChange={handleInputChange}
+								/>
+							</div>
+							<div className="flex justify-center items-center gap-1">
+								<label className="text-white min-w-50">Max X:</label>
+								<input
+									className="bg-gray-700 text-white rounded-md p-2"
+									type="text"
+									name="maxX"
+									value={minMaxValues.max_X}
+									onChange={handleInputChange}
+								/>
+							</div>
+							<div className="flex justify-center items-center gap-1">
+								<label className="text-white min-w-50">Min Y:</label>
+								<input
+									className="bg-gray-700 text-white rounded-md p-2"
+									type="text"
+									name="minY"
+									value={minMaxValues.min_Y}
+									onChange={handleInputChange}
+								/>
+							</div>
+							<div className="flex justify-center items-center gap-1">
+								<label className="text-white min-w-50">Max Y:</label>
+								<input
+									className="bg-gray-700 text-white rounded-md p-2"
+									type="text"
+									name="maxY"
+									value={minMaxValues.max_Y}
+									onChange={handleInputChange}
+								/>
+							</div>
+							<div className="flex justify-center items-center gap-1">
+								<label className="text-white min-w-50">Min Z:</label>
+								<input
+									className="bg-gray-700 text-white rounded-md p-2"
+									type="text"
+									name="minZ"
+									value={minMaxValues.min_Z}
+									onChange={handleInputChange}
+								/>
+							</div>
+							<div className="flex justify-center items-center gap-1">
+								<label className="text-white min-w-50">Max Z:</label>
+								<input
+									className="bg-gray-700 text-white rounded-md p-2"
+									type="text"
+									name="maxZ"
+									value={minMaxValues.min_Z}
+									onChange={handleInputChange}
+								/>
+							</div>
+						</div>
+						<button
+							type="submit"
+							className="btn bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded focus:outline-none focus:ring"
+						>
+							Show Results
+						</button>
+					</form>
 				</div>
 			)}
 		</div>
